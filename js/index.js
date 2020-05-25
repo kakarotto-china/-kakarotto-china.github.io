d66048c3df46720d2918b7d5079c5e46ef5b8a4d
kakarotto-china <kakarotto-china@users.noreply.github.com>
2020-05-25 19:09:38 +0800

    对验证添加cookie缓存

--- js/index.js
+++ js/index.js
@@ -2,17 +2,19 @@ let app = new Vue({
     el: "#app",
     data: {
         password: "",
-        message: ""
+        message: "",
     },
     methods: {
-        login(){
+        login() {
             encrypt = md5Encrypt(this.password, 10);
             console.log(encrypt)
-            if("734d5807701d8d462419b8edc04729c4" !== encrypt){
+            if (ciphertext !== encrypt) {
                 this.message = "验证失败"
                 return
             }
-            window.location.href = "/kakarotto-china.github.io/html/directories.html"
+            // 写入到cookie
+            setCookie("login", encrypt)
+            window.location.href = protocolPage
         },
     },
     filters: {},
@@ -24,13 +26,51 @@ let app = new Vue({
     },
 })
 
-let md5Encrypt = function (word, times) {
-    if(times<=0){
+
+let ciphertext = "734d5807701d8d462419b8edc04729c4"
+let protocolPage = "/kakarotto-china.github.io/html/directories.html"
+
+window.onload = function () {
+    login = getCookie("login")
+    if (login === ciphertext) {
+        window.location.href = protocolPage
+    }
+}
+
+// md5加密
+let md5Encrypt = function (word, times = 1) {
+    if (times <= 0) {
         return
     }
     let retStr = word
-    for(let i=0; i<times; i++){
+    for (let i = 0; i < times; i++) {
         retStr = CryptoJS.MD5(retStr).toString()
     }
     return retStr
+}
+
+// 写cookies
+let setCookie = function (name, value, days = 30) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+
+// 读取cookies
+let getCookie = function (name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+
+    if (arr = document.cookie.match(reg)) {
+        return unescape(arr[2]);
+    } else {
+        return null;
+    }
+}
+
+// 删除cookies
+let delCookie = function (name) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = getCookie(name);
+    if (cval != null) document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
 }
