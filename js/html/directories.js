00179671cb086d718168b43035999185e9f17154
kakarotto-china <kakarotto-china@users.noreply.github.com>
2020-05-28 16:25:33 +0800

    抽取目录页内容json到json文件中,并在window onload时加载

--- js/html/directories.js
+++ js/html/directories.js
@@ -1,73 +1,7 @@
 let app = new Vue({
     el: "#app",
     data: {
-        contentList: [
-            {
-                category: "搜索",
-                content: [
-                    {
-                        name: "Baidu",
-                        url: "http://baidu.com",
-                        desc: "百度"
-                    },
-                    {
-                        name: "Google",
-                        url: "http://google.com",
-                        desc: "谷歌"
-                    },
-                ]
-            },
-            {
-                category: "影音直播",
-                content: [
-                    {
-                        name: "B站",
-                        url: "http://bilibili.com",
-                        desc: "哔哩哔哩 视频弹幕网站"
-                    },
-                    {
-                        name: "斗鱼",
-                        url: "http://douyu.com",
-                        desc: "斗鱼 直播平台"
-                    },
-                    {
-                        name: "虎牙",
-                        url: "http://huya.com",
-                        desc: "虎牙 直播平台"
-                    },
-                    {
-                        name: "永恒de草薙",
-                        url: "https://live.ixigua.com/room/6831486448986753800/",
-                        desc: "西瓜视频 永恒[三国]直播"
-                    },
-                ]
-            },
-            {
-                category: "开发者",
-                content: [
-                    {
-                        name: "Github",
-                        url: "http://github.com",
-                        desc: "开源 代码托管平台"
-                    },
-                    {
-                        name: "码云",
-                        url: "http://gitee.com",
-                        desc: "国内开源 代码托管平台"
-                    },
-                ]
-            },
-            {
-                category: "自定义",
-                content: [
-                    {
-                        name: "Joy Controller",
-                        url: "./game/controller.html",
-                        desc: "游戏手柄控制器 调试"
-                    },
-                ]
-            },
-        ],
+        contentList: []
     },
     methods: {
         goto(url) {
@@ -81,3 +15,24 @@ let app = new Vue({
     updated() {
     }
 })
+
+window.onload = function () {
+    //usage:
+    readTextFile("../resources/json/contentList.json", function (text) {
+        let contentList = JSON.parse(text);
+        console.log(contentList);
+        app.contentList = contentList;
+    });
+}
+
+let readTextFile = function (file, callback) {
+    let rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
